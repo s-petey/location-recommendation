ca67302e6f75a257247c986f2b0e7ce23b64d5e0
Sam Peterson <samllama55@gmail.com>
2025-02-07 22:45:47 +0100

    FIX: Remove session cookie.

--- app/routes/logout.tsx
+++ app/routes/logout.tsx
@@ -1,7 +1,8 @@
 import { createFileRoute, redirect } from '@tanstack/react-router';
 import { createServerFn } from '@tanstack/start';
-import { getWebRequest } from '@tanstack/start/server';
+import { getEvent, getWebRequest, setCookie } from '@tanstack/start/server';
 import { auth } from '~/lib/auth';
+import { createCookieForServerAuth } from '~/lib/auth.helpers';
 
 const logoutFn = createServerFn().handler(async () => {
   const req = getWebRequest();
@@ -12,11 +13,21 @@ const logoutFn = createServerFn().handler(async () => {
 
   const resp = await auth.api.signOut({
     headers: req.headers,
+    asResponse: true,
   });
-  // TODO: Manually remove cookie? --verify I don't need to do such
 
-  if (!resp.success) {
+  if (!resp.ok) {
     throw new Error('Something went wrong signing out!');
+  }
+
+  const headers = resp.headers;
+
+  const cookiesToSet = createCookieForServerAuth(headers);
+  const event = getEvent();
+
+  for (const { key, value, options } of cookiesToSet) {
+    // "set" here a cookie with an immediate timeout.
+    setCookie(event, key, value, options);
   }
 
   throw redirect({
