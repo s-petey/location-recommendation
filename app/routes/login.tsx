c170fc75ae9ed13044a839b3207d5fe52b1c1cd4
Sam Peterson <samllama55@gmail.com>
2025-03-08 16:24:11 +0100

    ADD: Ignored files to biome. CHANGE: Run fix command.

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,5 +1,5 @@
 import { useMutation } from '@tanstack/react-query';
-import { createFileRoute, Link, useRouter } from '@tanstack/react-router';
+import { Link, createFileRoute, useRouter } from '@tanstack/react-router';
 import { createServerFn } from '@tanstack/start';
 import { getEvent, setCookie } from '@tanstack/start/server';
 import { type } from 'arktype';
